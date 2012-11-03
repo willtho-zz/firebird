36024852817453fb56b3b281a41be468606c99ff
Will <willtho1@umbc.edu>
2012-11-03 15:48:50 -0400

    adding favicon.ico code to serve

--- shop_server.py
+++ shop_server.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def icon():
+    return redirect( url_for( "static", filename="favicon.ico" ) )
 
 @app.route("/")
 def index():
     return render_template( "index.html" )
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
