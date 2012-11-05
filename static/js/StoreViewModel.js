3dca2e25019e5779827b128d952fa7d7d5dcde2f
Patrick O'Connell <poconnell93@gmail.com>
2012-11-04 19:20:27 -0500

    add index.html

---
+++ static/js/StoreViewModel.js
@@ -0,0 +1,66 @@
+function StoreViewModel() {
+	var self = this;
+
+	// store data
+	self.cart = ko.observableArray([]);
+	self.categories = ko.observableArray([]);
+
+	// ui state
+	self.category = ko.observable("");
+	self.search = ko.observable("");
+	self.screen = ko.observable("");
+
+	// navigation behaviors
+	self.goToAllCategories = function() {
+		location.hash = "";
+	}
+	self.goToCart = function() {
+		location.hash = "cart";
+	}
+	self.goToCategory = function(category) {
+		location.hash = "category/" + category;
+	}
+
+	// search behaviors
+	self.startSearch = function(text) {
+		self.goToCategory(self.category());
+		self.search(text);
+	}
+	self.clearSearch = function() {
+		self.search("");
+	}
+
+	// test data
+	self.cart(["item", "item", "item"]);
+	self.categories(["Books", "Card Magic", "Coin Magic", "Juggling", "Rope Magic"]);
+	self.removeItem = function() {
+		self.cart.pop();
+	}
+	self.addItem = function() {
+		self.cart.push("item");
+	}
+
+	// url hash routing
+	Sammy(function() {
+		this.get("#cart", function() {
+			self.category("");
+			self.screen("cart");
+		});
+		this.get("#category/:category", function() {
+			self.category(this.params.category);
+			self.screen("shop");
+		});
+		this.get("", function() {
+			self.category("");
+			self.screen("shop");
+		});
+	}).run();
+}
+
+StoreViewModel.formatItemCount = function(items) {
+	switch (items) {
+		case 0:  return "empty";
+		case 1:  return "1 item";
+		default: return items + " items";
+	}
+};
