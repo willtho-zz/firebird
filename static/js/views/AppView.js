1126a362438cc84d8f5e0adbd7a453447e892e5d
Patrick O'Connell <poconnell93@gmail.com>
2012-11-07 23:32:28 -0500

    add category list to sidebar

--- static/js/views/AppView.js
+++ static/js/views/AppView.js
@@ -9,25 +9,41 @@ firebird.AppView = Backbone.View.extend({
 
 		// cache HTML elements
 		self.$cartItemCount = self.$("#cartItemCount");
+		self.$categoryList = self.$("#categoryList");
 
 		// update cart item count
 		firebird.cart.on("all", function() {
 			self.$cartItemCount.html(firebird.cart.getFormattedCount());
 		});
 		firebird.cart.trigger("change");
+
+		// update category list
+		firebird.categories.on("add remove reset", function() {
+			self.$categoryList.html("<a id='shop' href='#shop' class='dark'>All Items</a><br>");
+			_.each(firebird.categories.models, function(category) {
+				var id = category.get("id");
+				self.$categoryList.append("<br><a id='shop-" + id + "' href='#shop/" + id +
+					"' class='dark'>" + category.get("name") + "</a>");
+			});
+		});
 	},
 
 	// navigation actions
 	navigateAllCategories: function() {
-		console.log("all categories");
+		this.setCategory("shop");
 	},
 
 	navigateCart: function() {
-		console.log("cart");
+		this.setCategory();
 	},
 
 	navigateCategory: function(id) {
-		console.log("category " + id);
+		this.setCategory("shop-" + id);
+	},
+
+	// change the link for the given category to bold
+	setCategory: function(id) {
+		this.$categoryList.find("a").removeClass("bold").filter("#" + id).addClass("bold");
 	}
 
 });
