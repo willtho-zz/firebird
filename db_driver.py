2b0b97fd9f5b660ad18cbf012962577faae1eed7
Will <willtho1@umbc.edu>
2012-11-12 12:42:48 -0500

    adding database code

--- db_driver.py
+++ db_driver.py
@@ -23,19 +23,38 @@ class database(object):
         pass
 
     def add(self, item):
-        pass
-
+        cursor = self.connect()
+        cursor.execute( "INSERT INTO inventory( name, category, quantity, price, salePrice, description) VALUES ('{}', '{}', {}, {}, {}, '{}')".format( item['name'], item['category'], item['quantity'], item['price'], item['salePrice'], item['description'] ) )
+        cursor.close()
+        self.conn.commit()
+        
     def remove(self, uid):
-        pass
+        cursor = self.connect()
+        cursor.execute( "DELETE FROM inventory WHERE id = {}".format( uid ) )
+        cursor.close()
+        self.conn.commit()
 
     def edit(self, item):
         pass
 
     def get_item(self, uid):
-        pass
+        cursor = self.connect()
+        tmp = cursor.execute( "Select * from inventory where id = {}".format( uid ) )
+        cursor.close()
+        self.conn.commit()
+        tmp = tmp.fetchall()[0]
+        return tmp
+
 
     def get_items(self, category=None):
-        pass
+        cursor = self.connect()
+        tmp = None
+        if category == None:
+            tmp = cursor.execute( "SELECT * FROM inventory" )
+        else:
+            tmp = cursor.execute( "SELECT * FROM inventory where category = {}".format( category ) )
+        cursor.close()
+        return tmp.fetchall()
 
     def get_categories(self):
         return [{"id":1, "name": "test1"}, {"id":2, "name":"test2"}]
