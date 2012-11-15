3a4e3857d9326b7d3010b76a7759388f1f0313f7
Patrick O'Connell <poconnell93@gmail.com>
2012-11-14 22:31:31 -0500

    add pagination in inventory view

--- static/js/views/InventoryView.js
+++ static/js/views/InventoryView.js
@@ -18,7 +18,7 @@ firebird.InventoryView = Backbone.View.extend({
     var category = self.category ? firebird.categories.get(self.category).get("name") : "All Items";
 
     // create the item list
-    var items = firebird.inventory.filter({ category: self.category, query: self.query }), rows = [];
+    var items = firebird.inventory.filter({ category: self.category, query: self.query }), rows = [], pages = [];
 
     // split the item array into groups of four
     while (items.length) {
@@ -26,13 +26,35 @@ firebird.InventoryView = Backbone.View.extend({
       items = _(items).rest(4);
     }
 
-    self.$el.html(self.inventoryViewTemplate({ category: category, query: self.query, items: rows }));
+    // split the rows into pages (groups of three)
+    while (rows.length) {
+      pages = pages.concat([_(rows).first(3)]);
+      rows = _(rows).rest(3);
+    }
+
+    self.$el.html(self.inventoryViewTemplate({
+      category: category,
+      query: self.query,
+      currentPage: self.page,
+      pages: Math.max(pages.length, 1),
+      items: pages[self.page - 1]
+    }));
 
     // set up event handlers
     setTimeout(function() {
       // "remove" link
       self.$el.find("#removeSearch").click(function(e) {
         firebird.router.navigate("shop/" + self.category + "/p1", { trigger: true });
+        e.preventDefault();
+      });
+
+      // page links
+      self.$el.find(".navPage").click(function(e) {
+        // construct the url based on whether a search is active and the link's page
+        var url = self.query ? "search/" + self.query : "shop";
+        url += "/" + self.category + "/p" + $(this).data("page");
+
+        firebird.router.navigate(url, { trigger: true });
         e.preventDefault();
       });
     }, 10);
