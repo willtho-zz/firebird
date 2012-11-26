c5baab930cedb685b2ab304cad26c95e38b5d5e1
Patrick O'Connell <poconnell93@gmail.com>
2012-11-25 19:53:06 -0500

    add total price and update button to cart

--- static/js/views/CartView.js
+++ static/js/views/CartView.js
@@ -21,8 +21,19 @@ firebird.CartView = Backbone.View.extend({
 
     setTimeout(function() {
       // add the remove button event handler
-      self.$(".removeButton").click(function(e) {
+      self.$(".removeButton").click(function() {
         firebird.cart.remove(firebird.cart.where({ itemID: $(this).data("item-id") }));
+        firebird.app.navigateCart();
+      });
+
+      // update button
+      self.$("#updateButton").click(function() {
+        self.$(".quantityInput").each(function() {
+          var id = $(this).data("item-id");
+
+          firebird.cart.where({ itemID: id })[0].set("count", parseInt($(this).val()));
+        });
+
         firebird.app.navigateCart();
       });
     }, 350);
