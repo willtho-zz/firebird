ee556c068c6ee89337f30181fe8267f74bab016b
Nathan Hartmann <nhartm1@umbc.edu>
2012-11-27 15:30:15 -0500

    Roman added

--- loaditems.sql
+++ loaditems.sql
@@ -39,6 +39,7 @@ INSERT INTO inventory(name, category, quantity, price, salePrice, description) V
 INSERT INTO inventory(name, category, quantity, price, salePrice, description) VALUES('Coin in the Balloon', 4, 5, 20.00, 20.00, 'The key here is to put the coin inside the balloon before you blow it up. There, I saved you like 20 bucks.');
 INSERT INTO inventory(name, category, quantity, price, salePrice, description) VALUES('A Wand', 3, 5, 10.00, 9.00, 'Need a wizard hat...');
 INSERT INTO inventory(name, category, quantity, price, salePrice, description) VALUES('The Force', 2, 5, 1000.00, 900.00, 'The force is strong with this one.');
+INSERT INTO inventory(name, category, quantity, price, salePrice, description) VALUES('Magic Lessons', 7, 5, 5.00, 3.00, 'Will work for food.');
 INSERT INTO categories(category) VALUES ('Bottles');
 INSERT INTO categories(category) VALUES ('Hats');
 INSERT INTO categories(category) VALUES ('Wands');
