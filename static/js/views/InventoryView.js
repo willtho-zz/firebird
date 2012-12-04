79156f0ddb3d7f5dd2817ccbf98100ae417f127f
Patrick O'Connell <poconnell93@gmail.com>
2012-12-04 14:12:35 -0500

    add item removal and changing category

--- static/js/views/InventoryView.js
+++ static/js/views/InventoryView.js
@@ -37,7 +37,8 @@ firebird.InventoryView = Backbone.View.extend({
       query: self.query,
       currentPage: self.page,
       pages: Math.max(pages.length, 1),
-      items: pages[self.page - 1] || []
+      items: pages[self.page - 1] || [],
+      admin: firebird.app.loggedIn
     }));
 
     // set up event handlers
@@ -62,6 +63,55 @@ firebird.InventoryView = Backbone.View.extend({
       self.$el.find(".itemLink").click(function(e) {
         firebird.router.navigate("item/" + $(this).data("item-id"), { trigger: true });
         e.preventDefault();
+      });
+
+      // "delete item" links
+      self.$el.find(".deleteItemLink").click(function(e) {
+        e.preventDefault();
+
+        $.ajax("/api/inventory/" + $(this).data("item-id"), {
+          type: "DELETE",
+          success: function() {
+            Notifier.success("Item deleted.");
+            firebird.inventory.fetch();
+          }
+        }).error(function() {
+          Notifier.error("Could not delete item.");
+        });
+      });
+
+      // "move item" links
+      self.$el.find(".moveItemLink").click(function(e) {
+        e.preventDefault();
+
+        var div = $("<span class='popupMenu'><ul></ul></span>"), itemID = $(this).data("item-id"),
+            ul = div.find("ul");
+
+        firebird.categories.each(function(category) {
+          var categoryID = category.get("id");
+          ul.append("<li><a href='' class='dark'>" + category.get("name") + "</a></li>")
+            .find("a").last().click(function(e) {
+              e.preventDefault();
+
+              var item = _.clone(firebird.inventory.get(itemID));
+              item.category = categoryID;
+
+              firebird.inventory.get(itemID).save({ category: categoryID }, {
+                success: function() { Notifier.success("Item moved."); },
+                error: function() {
+                  Notifier.error("Could not move item.");
+                  firebird.inventory.trigger("reset");
+                }
+              });
+            });
+        });
+
+        div.mouseleave(function() {
+          div.hide().detach();
+        });
+
+        $("body").append(div);
+        div.position({ my: "left top", at: "left top", of: this });
       });
     }, 10);
 
