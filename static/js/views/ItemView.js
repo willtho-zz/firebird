ffca302fa50fed8a28597203dce8ad415b814e95
Patrick O'Connell <poconnell93@gmail.com>
2012-12-09 16:47:19 -0500

    add edit dialog

--- static/js/views/ItemView.js
+++ static/js/views/ItemView.js
@@ -14,7 +14,10 @@ firebird.ItemView = Backbone.View.extend({
   render: function() {
     var self = this;
 
-    self.$el.html(self.itemViewTemplate({ item: firebird.inventory.get(self.id).attributes }));
+    self.$el.html(self.itemViewTemplate({
+      item: firebird.inventory.get(self.id).attributes,
+      admin: firebird.app.loggedIn
+    }));
 
     // set up event handlers
     setTimeout(function() {
@@ -41,6 +44,58 @@ firebird.ItemView = Backbone.View.extend({
         }
 
         Notifier.success("\"" + firebird.inventory.get(self.id).get("name") + "\" added to cart.");
+      });
+
+      // edit button
+      self.$("#editLink").click(function(e) {
+        e.preventDefault();
+
+        var dialog = firebird.modalDialog2("Edit Item",
+          "<table><tr><td>Name:</td><td><input id='itemName'></td></tr>" +
+          "<tr><td>Description:</td><td><input id='itemDescription'></td></tr>" +
+          "<tr><td>Category:</td><td><select id='itemCategory'></select></td></tr>" +
+          "<tr><td>Price:</td><td><input id='itemPrice'></td></tr>" +
+          "<tr><td>Sale Price:</td><td><input id='itemSalePrice'></td></tr>" +
+          "<tr><td>Quantity:</td><td><input id='itemQuantity'></td></tr></table>",
+          {
+            Edit: function() {
+              firebird.inventory.get(self.id).save({
+                name: dialog.find("#itemName").val(),
+                description: dialog.find("#itemDescription").val(),
+                category: dialog.find("#itemCategory").val(),
+                price: parseFloat(dialog.find("#itemPrice").val()),
+                salePrice: parseFloat(dialog.find("#itemSalePrice").val()),
+                quantity: parseInt(dialog.find("#itemQuantity").val())
+              }, {
+                success: function() {
+                  Notifier.success("Item edit.");
+                  firebird.inventory.fetch();
+                  dialog.dialog("close");
+                },
+                error: function() {
+                  Notifier.error("Could not edit item.");
+                  dialog.dialog("close");
+                }
+              });
+            },
+            Cancel: function() {
+              dialog.dialog("close");
+            }
+          });
+
+        firebird.categories.each(function(category) {
+          dialog.find("#itemCategory").append("<option value='" + category.get("id") +
+                                              "'>" + category.get("name") + "</option>");
+
+        var item = firebird.inventory.get(self.id);
+
+        dialog.find("#itemName").val(item.get("name"));
+        dialog.find("#itemDescription").val(item.get("description"));
+        dialog.find("#itemCategory").val(item.get("category"));
+        dialog.find("#itemPrice").val(item.get("price"));
+        dialog.find("#itemSalePrice").val(item.get("salePrice"));
+        dialog.find("#itemQuantity").val(item.get("quantity"));
+        });
       });
     }, 10);
 
